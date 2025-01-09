a4d23f1551f842d140a225eca71aec9f2b855cb4
luisgcode <luisgcode@gmail.com>
2025-01-09 17:43:46 -0500

    Fixing vite gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/portfolio2.0/", // Cambia 'portfolio2.0' al nombre exacto de tu repositorio
+});
