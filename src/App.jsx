d70d92db2d05f832db8ecde6aa48a774cb5f94ac
luisgcode <luisgcode@gmail.com>
2025-01-08 19:04:18 -0500

    More setting up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App gradient-bg">
-      <h1 className="text-red-400"> Vasdfite + React</h1>
+      <h1 className="text-red-400"> Vasddfite + React</h1>
+      <li>sdf</li>
     </div>
   );
 }
