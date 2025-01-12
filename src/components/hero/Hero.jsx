f3229cadfd3393c7a9b35997575fbf4cdb716764
luisgcode <luisgcode@gmail.com>
2025-01-12 17:13:45 -0500

    Building apply css rules

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -27,33 +27,23 @@ const Hero = () => {
       >
         <div className="intro   ">
           <h1>Luis Guaiquirian</h1>
-          <h2 className="text-highlightColor  ">Web Developer</h2>
+          <h2 className="text-highlightColor">Web Developer</h2>
           <p className="mt-1 mb-6 max-w-[35ch]">
             I build responsive Web applications with modern technologies,
             specializing in:
           </p>
           <div className="flex flex-wrap items-center gap-x-4 gap-y-3 md:gap-x-5">
-            <span className=" px-2 rounded-lg py-1 bg-highlightColor bg-opacity-30 border border-highlightColor hover:bg-opacity-0 transition-all duration-300">
-              React JS
-            </span>
-            <span className=" px-2 rounded-lg py-1 bg-highlightColor bg-opacity-30 border border-highlightColor hover:bg-opacity-0 transition-all duration-300 ">
-              Tailwind CSS
-            </span>{" "}
-            <span className=" px-2 rounded-lg py-1 bg-highlightColor bg-opacity-30 border border-highlightColor hover:bg-opacity-0 transition-all duration-300 text-sm">
-              JavaScript
-            </span>{" "}
-            <span className=" px-2 rounded-lg py-1 bg-highlightColor bg-opacity-30 border border-highlightColor hover:bg-opacity-0 transition-all duration-300 text-xs">
-              Next JS
-            </span>
-            <span className=" px-2 rounded-lg py-1 bg-highlightColor bg-opacity-30 border border-highlightColor hover:bg-opacity-0 transition-all duration-300 text-xs">
-              Python
-            </span>
+            <span className="custom-hero-tag ">React JS</span>
+            <span className="custom-hero-tag ">Tailwind CSS</span>
+            <span className="custom-hero-tag text-sm">JavaScript</span>
+            <span className="custom-hero-tag text-xs">Next JS</span>
+            <span className="custom-hero-tag text-xs">Python</span>
           </div>
           <div className="mt-6">
             <ul className="flex gap-6">
               <li>
                 <a
-                  className="flex gap-1 text-1xl hover:scale-110 transition-scale duration-100"
+                  className="hero-icon hover:scale-110 transition-scale duration-100"
                   href="/portfolio2.0/cv_developer_luis.pdf"
                   download="luis_summary.pdf"
                 >
@@ -62,7 +52,7 @@ const Hero = () => {
               </li>
               <li>
                 <a
-                  className="flex gap-1 text-1xl hover:scale-110 transition-scale duration-100"
+                  className="hero-icon hover:scale-110 transition-scale duration-100"
                   href="https://www.linkedin.com/in/luisgcode/"
                   target="_blank"
                 >
@@ -71,7 +61,7 @@ const Hero = () => {
               </li>
               <li>
                 <a
-                  className="flex gap-1 text-1xl hover:scale-110 transition-scale duration-100"
+                  className="hero-icon hover:scale-110 transition-scale duration-100"
                   href="https://github.com/luisgcode"
                   target="_blank"
                 >
@@ -81,7 +71,7 @@ const Hero = () => {
             </ul>
           </div>
         </div>
-        <div className=" hidden md:flex items-center image   lg:mr-16  ">
+        <div className=" hidden md:flex items-center   lg:mr-16  ">
           <img className="w-[280px] " src={heroImage} alt="Luis Guaiquirian" />
         </div>
       </motion.div>
