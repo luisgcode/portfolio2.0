3214a4035a816d6e947c4eb002ebc24330ce42e0
luisgcode <luisgcode@gmail.com>
2025-01-13 23:39:05 -0500

    Starting projects table

--- src/pages/ProjectsPage/tableList/TableList.jsx
+++ src/pages/ProjectsPage/tableList/TableList.jsx
@@ -4,7 +4,118 @@ import "./tablelist.css";
 const TableList = () => {
   return (
     <div>
-      <div className="p-sma_pad md:p-mid_pad">dfsd</div>
+      <div className="p-sma_pad md:p-mid_pad">
+        <div className="table">
+          <table>
+            <tr className="table-heading-row">
+              <th>Year</th>
+              <th>Project</th>
+              <th>For</th>
+              <th>Tech Stack</th>
+              <th>Link</th>
+            </tr>
+
+            <tr className="table-data-row">
+              <td>2025</td>
+              <td>luisgcode.com (v2)</td>
+              <td>My Website</td>
+              <td>
+                <span className="custom-project-tag">React JS</span>
+                <span className="custom-project-tag">Tailwind CSS</span>
+                <span className="custom-project-tag">Framer Motion</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">SPA</span>
+                <span className="custom-project-tag">Bilingual</span>
+                <span className="custom-project-tag">Accessible</span>
+              </td>
+              <td>www.luisgcode.com</td>
+              <td></td>
+            </tr>
+            <tr className="table-data-row">
+              <td>2025</td>
+              <td>luisgcode.com (v2)</td>
+              <td>My Website</td>
+              <td>
+                <span className="custom-project-tag">React JS</span>
+                <span className="custom-project-tag">Tailwind CSS</span>
+                <span className="custom-project-tag">Framer Motion</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">SPA</span>
+                <span className="custom-project-tag">Bilingual</span>
+                <span className="custom-project-tag">Accessible</span>
+              </td>
+              <td>www.luisgcode.com</td>
+              <td></td>
+            </tr>
+            <tr className="table-data-row">
+              <td>2025</td>
+              <td>luisgcode.com (v2)</td>
+              <td>My Website</td>
+              <td>
+                <span className="custom-project-tag">React JS</span>
+                <span className="custom-project-tag">Tailwind CSS</span>
+                <span className="custom-project-tag">Framer Motion</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">SPA</span>
+                <span className="custom-project-tag">Bilingual</span>
+                <span className="custom-project-tag">Accessible</span>
+              </td>
+              <td>www.luisgcode.com</td>
+              <td></td>
+            </tr>
+            <tr className="table-data-row">
+              <td>2025</td>
+              <td>luisgcode.com (v2)</td>
+              <td>My Website</td>
+              <td>
+                <span className="custom-project-tag">React JS</span>
+                <span className="custom-project-tag">Tailwind CSS</span>
+                <span className="custom-project-tag">Framer Motion</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">SPA</span>
+                <span className="custom-project-tag">Bilingual</span>
+                <span className="custom-project-tag">Accessible</span>
+              </td>
+              <td>www.luisgcode.com</td>
+              <td></td>
+            </tr>
+            <tr className="table-data-row">
+              <td>2025</td>
+              <td>luisgcode.com (v2)</td>
+              <td>My Website</td>
+              <td>
+                <span className="custom-project-tag">React JS</span>
+                <span className="custom-project-tag">Tailwind CSS</span>
+                <span className="custom-project-tag">Framer Motion</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">SPA</span>
+                <span className="custom-project-tag">Bilingual</span>
+                <span className="custom-project-tag">Accessible</span>
+              </td>
+              <td>www.luisgcode.com</td>
+              <td></td>
+            </tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
