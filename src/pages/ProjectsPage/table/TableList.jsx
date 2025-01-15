b675fb9076be36a4679b1daeecff0ac3cba69888
luisgcode <luisgcode@gmail.com>
2025-01-15 10:55:06 -0500

    Progressing projects archive

--- src/pages/ProjectsPage/table/TableList.jsx
+++ src/pages/ProjectsPage/table/TableList.jsx
@@ -1,238 +1,159 @@
 import React from "react";
 import "./table.css";
 import { FiArrowUpRight } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
 
 const TableList = () => {
   return (
     <div>
       <div className="p-sma_pad md:p-mid_pad">
-        <div className="table">
-          <table className="table">
-            {/* Table heading */}
-            <thead className="text-left">
-              <tr>
-                <th>Year</th>
-                <th>Project</th>
-                <th>For</th>
-                <th>Tech Stack</th>
-                <th>Link</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* portfolio v2 */}
-              <tr className="m-4">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td className="link text-sm">
-                  <a href="https://www.luisgcode.com/" className="flex">
-                    luisgcode <FiArrowUpRight />
-                  </a>
-                </td>
-              </tr>
-              {/* Portfolio v1 */}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-              {/* RBC place Hub */}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-              {/* Global Technologies */}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-              {/* Crowd meter */}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-              {/* Our stores Infanti*/}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-              {/* Terms and conditions Infanti*/}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-              {/* offers timer Infanti*/}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-              {/* Can jet Pattern Library & Dashboard */}
-              <tr className="">
-                {/* Year */}
-                <td>2025</td>
-                {/* Project */}
-                <td>luisgcode.com (v2)</td>
-                {/* for */}
-                <td>My Website</td>
-                {/* Tags */}
-                <td>
-                  <span className="custom-project-tag">React JS</span>
-                  <span className="custom-project-tag bg-red-300">
-                    Tailwind CSS
-                  </span>
-                  <span className="custom-project-tag">Framer Motion</span>
-                  <span className="custom-project-tag">Responsive</span>
-                  <span className="custom-project-tag">SPA</span>
-                  <span className="custom-project-tag">Bilingual</span>
-                  <span className="custom-project-tag">Accessible</span>
-                </td>
-                {/* link */}
-                <td>www.luisgcode.com</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <table className="table">
+          {/* Table heading */}
+          <thead className="text-left">
+            <tr>
+              <th>Year</th>
+              <th>Project</th>
+
+              <th>Tech Stack</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* portfolio v2 */}
+            <tr>
+              {/* Year */}
+              <td>2025</td>
+              {/* Project */}
+              <td className="font-semibold">Portfolio (v2)</td>
+              {/* Tags */}
+              <td className="tags-td">
+                <span className="custom-project-tag">React JS</span>
+                <span className="custom-project-tag bg-red-300">
+                  Tailwind CSS
+                </span>
+                <span className="custom-project-tag">Framer Motion</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">SPA</span>
+                <span className="custom-project-tag">Bilingual</span>
+                <span className="custom-project-tag">Accessible</span>
+              </td>
+              {/* link */}
+              <td className="link ">
+                <a
+                  href="https://www.luisgcode.com/"
+                  target="_blank"
+                  className="flex"
+                >
+                  luisgcode.com <FiArrowUpRight />
+                </a>
+              </td>
+            </tr>
+            {/* Global Technologies */}
+            <tr>
+              {/* Year */}
+              <td>2024</td>
+              {/* Project */}
+              <td className="font-semibold">
+                Global Technologies - Optimization & Functionalities
+              </td>
+              {/* Tags */}
+              <td className="tags-td">
+                <span className="custom-project-tag">TypeScript</span>
+                <span className="custom-project-tag">JavaScript</span>
+                <span className="custom-project-tag">Responsive</span>
+              </td>
+              {/* link */}
+              <td className="link ">
+                <a
+                  href="https://globaltechnologies.web.app/"
+                  target="_blank"
+                  className="flex"
+                >
+                  globaltechnologies.web.app <FiArrowUpRight />
+                </a>
+              </td>
+            </tr>
+            {/* Can jet Pattern Library & Dashboard */}
+            <tr>
+              {/* Year */}
+              <td>2024</td>
+              {/* Project */}
+              <td className="font-semibold">Pattern Library & Dashboard</td>
+              {/* Tags */}
+              <td className="tags-td">
+                <span className="custom-project-tag">D3.js</span>
+                <span className="custom-project-tag">JavaScript</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">CSS</span>
+              </td>
+              {/* link */}
+              <td className="link ">
+                <a
+                  href="https://github.com/luisgcode/portfolio2024"
+                  target="_blank"
+                  className="flex gap-2"
+                >
+                  GitHub
+                  <FaGithub />
+                </a>
+              </td>
+            </tr>
+            {/* Portfolio v1 */}
+            <tr>
+              {/* Year */}
+              <td>2024</td>
+              {/* Project */}
+              <td className="font-semibold">Portfolio (v1)</td>
+              {/* Tags */}
+              <td className="tags-td">
+                <span className="custom-project-tag">Sass</span>
+                <span className="custom-project-tag">JavaScript</span>
+                <span className="custom-project-tag">Responsive</span>
+                <span className="custom-project-tag">Bilingual</span>
+                <span className="custom-project-tag">Accessible</span>
+              </td>
+              {/* link */}
+              <td className="link ">
+                <a
+                  href="https://github.com/luisgcode/portfolio2024"
+                  target="_blank"
+                  className="flex gap-2"
+                >
+                  GitHub
+                  <FaGithub />
+                </a>
+              </td>
+            </tr>
+            {/* RBC place Hub */}
+            <tr>
+              {/* Year */}
+              <td>2024</td>
+              {/* Project */}
+              <td className="font-semibold">RBC Place - Information HUB</td>
+              {/* Tags */}
+              <td className="tags-td">
+                <span className="custom-project-tag">HTML</span>
+                <span className="custom-project-tag">CSS</span>
+                <span className="custom-project-tag">JavaScript</span>
+              </td>
+              {/* link */}
+              <td className="link ">
+                <a
+                  href="https://www.rbcplacelondon.com/"
+                  target="_blank"
+                  className="flex"
+                >
+                  rbcplacelondon.com
+                  <FiArrowUpRight />
+                </a>
+              </td>
+            </tr>
+            {/* Crowd meter */}
+            {/* Our stores Infanti*/}
+            {/* Terms and conditions Infanti*/}
+            {/* offers timer Infanti*/}
+          </tbody>
+        </table>
       </div>
     </div>
   );
