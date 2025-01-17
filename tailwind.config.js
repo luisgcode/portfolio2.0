1d76c52b9295e843d45da42d252925d3856807f8
luisgcode <luisgcode@gmail.com>
2025-01-17 11:25:37 -0500

    Adjusting padding mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
         highlightColor: "#9698F3",
       },
       spacing: {
-        sma_pad: "24px 32px",
+        sma_pad: "24px 24px",
         mid_pad: "40px 120px",
       },
     },
